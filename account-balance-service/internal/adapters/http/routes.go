9c2dce6d5f734954838a014cc74f11936707d821
Vitor Vasconcellos <vitor.vasconcellos@mercadolivre.com>
2024-07-03 09:28:52 -0300

    Add ping endpoints

--- account-balance-service/internal/adapters/http/routes.go
+++ account-balance-service/internal/adapters/http/routes.go
@@ -7,7 +7,17 @@ import (
 )
 
 func MapRoutes(r *gin.Engine) {
+	r.GET("/ping", ping())
+
 	r.GET("/v1/users/:userId/account_balance", metrics.RecordMetrics(getAccountBalance()))
 	r.POST("/v1/users/:userId/account_balance/debit", metrics.RecordMetrics(createDebit()))
 	r.GET("/v1/users/:userId", metrics.RecordMetrics(getUser()))
+}
+
+func ping() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	}
 }
--- transactions-service/internal/adapters/http/routes.go
+++ transactions-service/internal/adapters/http/routes.go
@@ -7,7 +7,17 @@ import (
 )
 
 func MapRoutes(r *gin.Engine) {
+	r.GET("/ping", ping())
+
 	r.POST("/v1/transactions", metrics.RecordMetrics(createTransaction()))
 	r.PUT("/v1/transactions/:transactionId", metrics.RecordMetrics(updateTransaction()))
 	r.GET("/v1/transactions/:userId/timeline", metrics.RecordMetrics(getUserTransactions()))
+}
+
+func ping() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	}
 }
